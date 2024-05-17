ce052518aee746825dd4563ce955c8aab1b4a8f5
RedondoDEV <javierRedondoDEV@gmail.com>
2024-05-17 21:39:24 +0200

    Log in verifier

--- src/gui/Vista.java
+++ src/gui/Vista.java
@@ -4,14 +4,19 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
+
+import bd.Prueba;
 
 public class Vista extends JFrame {
 
@@ -42,11 +47,13 @@ public class Vista extends JFrame {
 
 }
 
-class MiPanel extends JPanel {
+class MiPanel extends JPanel implements ActionListener {
 
 	JButton signIn, logIn;
 	JTextField nombre;
 	JPasswordField contrasena;
+	Image logo, logoU, logoC;
+	JLabel errorUsuario, errorContrasena;
 
 	public MiPanel() {
 
@@ -68,6 +75,12 @@ class MiPanel extends JPanel {
 		logIn.setBounds(200, 230, 80, 30);
 		this.add(signIn);
 		this.add(logIn);
+
+		errorUsuario = crearTextoError();
+		errorUsuario.setBounds(55, 112, 250, 100);
+		errorUsuario.setVisible(false);
+
+		this.add(errorUsuario);
 
 		this.setOpaque(true);
 
@@ -97,10 +110,17 @@ class MiPanel extends JPanel {
 		b.setMnemonic('s');
 		b.setBackground(new Color(Vista.COLOR3));
 		b.setBorder(BorderFactory.createLineBorder(new Color(Vista.COLOR1), 2, true));
+		b.addActionListener(this);
 		return b;
 	}
 
-	Image logo, logoU, logoC;
+	private JLabel crearTextoError() {
+		JLabel t = new JLabel();
+		t.setText("Debe tener entre 4 y 16 caracteres");
+		t.setForeground(Color.RED);
+		t.setFont(new Font("Consolas", Font.ITALIC, 13));
+		return t;
+	}
 
 	@Override
 	protected void paintComponent(Graphics g) {
@@ -112,6 +132,21 @@ class MiPanel extends JPanel {
 		g.drawImage(logo, 87, 45, 150, 50, null);
 		g.drawImage(logoU, 30, 133, 18, 18, null);
 		g.drawImage(logoC, 30, 183, 18, 18, null);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == logIn) {
+			boolean comprobado = Prueba.hayUsuario(nombre.getText(), String.valueOf(contrasena.getPassword()));
+			String nombreUsusario = nombre.getText();
+			String contrasenaUsuario = String.valueOf(contrasena.getPassword());
+			System.out.println(String.valueOf(contrasena.getPassword()));
+			if (!comprobado) {
+				errorUsuario.setVisible(true);
+			}
+		} else if (e.getSource() == signIn) {
+			System.out.println("Loged In");
+		}
 	}
 
 }
